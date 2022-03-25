7591b7470cf7c1ed6e171d8bb35a1bff318ccaa7
Mohamed El Mehdi <mkhalfoun@riverdone.digital>
2022-03-25 14:21:50 +0100

    added a route to post a new element

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const App = express();
 App.use(express.json());
 App.get("/", (req, res) => {
   res.status(200).json({
-    message: "success",
+    message: "Hello world",
   });
+});
+App.post("/add-element", (req, res) => {
+  res.status(200).json({ message: "Element Addes Successfully" });
 });
